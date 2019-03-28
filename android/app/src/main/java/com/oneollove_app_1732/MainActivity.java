040e01072e8c4e36ee814dee300964a76f3ebb8c
Crowdbotics <team@crowdbotics.com>
2019-03-28 23:38:41 +0000

    initial commit

---
+++ android/app/src/main/java/com/oneollove_app_1732/MainActivity.java
@@ -0,0 +1,15 @@
+package com.oneollove_app_1732;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "oneollove_app_1732";
+    }
+}
